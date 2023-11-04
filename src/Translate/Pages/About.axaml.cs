468aa8e35f4e629046ab78b17f7833aa7afd7444
token <239573049@qq.com>
2023-11-04 11:33:53 +0800

    优化页面

---
+++ src/Translate/Pages/About.axaml.cs
@@ -0,0 +1,16 @@
+using System;
+using Avalonia.Controls;
+using Avalonia.Input;
+
+namespace Token.Translate.Pages;
+public partial class About : UserControl
+{
+    public About()
+    {
+        InitializeComponent();
+    }
+
+    private void InputElement_OnPointerReleased(object? sender, PointerReleasedEventArgs e)
+    {
+    }
+}
